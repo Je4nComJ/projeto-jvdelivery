74108eb9030b018db6c9c2bca097fd020aab691c
Je4nComJ <jeanvictor.fa@gmail.com>
2024-07-15 18:40:31 -0300

    POST /orders

--- backend/src/main/java/com/gomesdevelopers/jvdelivery/services/OrderService.java
+++ backend/src/main/java/com/gomesdevelopers/jvdelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.gomesdevelopers.jvdelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gomesdevelopers.jvdelivery.dto.OrderDTO;
+import com.gomesdevelopers.jvdelivery.dto.ProductDTO;
 import com.gomesdevelopers.jvdelivery.entities.Order;
+import com.gomesdevelopers.jvdelivery.entities.OrderStatus;
+import com.gomesdevelopers.jvdelivery.entities.Product;
 import com.gomesdevelopers.jvdelivery.repositories.OrderRepository;
+import com.gomesdevelopers.jvdelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
